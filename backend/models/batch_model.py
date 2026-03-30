c8ee0d7268ea74e6d0513eaf23dfd09fcd8d1d8f
Arunachalamsubramanian <arunachalam5315@gmail.com>
2026-03-30 15:26:55 +0530

    hi

---
+++ backend/models/batch_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from database.database import Base
+
+class DrugBatch(Base):
+    __tablename__ = "drug_batches"
+
+    id = Column(Integer, primary_key=True, index=True)
+    batch_id = Column(String, unique=True, index=True)
+    expected_hash = Column(String)
+    drug_name = Column(String)
+    manufacturer = Column(String)
+    manufacturing_date = Column(String)
+    expiry_date = Column(String)
+    current_owner = Column(String, default="Manufacturer")
+    status = Column(String, default="Available")
