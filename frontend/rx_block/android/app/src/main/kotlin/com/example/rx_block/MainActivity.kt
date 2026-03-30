ac5a59825cc0bada194323f21b9aa8ec3f9d5537
Harshan07-web <bharshan17@gmail.com>
2026-03-31 02:32:34 +0530

    more files

--- frontend/rx_block/android/app/src/main/kotlin/com/example/rx_block/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rx_block
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/rx_block/lib/core/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTheme {
-  static final darkTheme = ThemeData(
-    brightness: Brightness.dark,
-    scaffoldBackgroundColor: Color(0xFF0A0F1E),
-    primaryColor: Colors.blueAccent,
-  );
-}
